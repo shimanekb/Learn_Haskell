b5ab22beb4efad09bbd989955b074e44fe19347f
shimanekb <brandon.shimanek@hotmail.com>
2015-10-07 12:50:08 -0500

    added ch2 type examples

---
+++ ch2/fntype.hs
@@ -0,0 +1,10 @@
+doubleUs x y = x * 2 + y * 2
+
+doubleMe x = x + x
+
+doubleSmallNumber x = if x > 100
+                        then x
+                        else x * 2
+
+removeNonUppercase :: [Char] -> [Char]
+removeNonUppercase st = [c | c <- st, c `elem` ['A'..'Z']]
